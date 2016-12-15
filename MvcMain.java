fc0b703a118acb72f73f49f4ae6cbcd4029dd22e
Robinhoets <robertets88@gmail.com>
2016-12-15 00:02:37 -0500

    update MvcMain

--- MvcMain.java
+++ MvcMain.java
@@ -3,11 +3,11 @@ import javax.swing.SwingUtilities;
 public class MvcMain {
 
    private static void createAndShowGui() {
-      //MvcView view = new ShowTextView("Show Text");
-      //MvcModel model = new ShowTextModel();
-      //Control control = newControl(view, model);
+      MvcView view = new ShowTextView("Show Text");
+      MvcModel model = new ShowTextModel();
+      Control control = newControl(view, model);
 
-      //control.showView(true);
+      control.showView(true);
    }
 
    public static void main(String[] args) {
