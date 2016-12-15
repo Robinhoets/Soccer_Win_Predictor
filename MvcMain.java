5cfe4a2c0e3913d6dcdd15c61d008277a04165c0
Robinhoets <robertets88@gmail.com>
2016-12-14 23:08:30 -0500

    updated MvcMain.java

--- MvcMain.java
+++ MvcMain.java
@@ -1,13 +1,13 @@
-import java.swing.SwingUtilities;
+import javax.swing.SwingUtilities;
 
 public class MvcMain {
 
    private static void createAndShowGui() {
-      MvcView view = new ShowTextView("Show Text");
-      MvcModel model = new ShowTextModel();
-      Control control = new Control(view, model);
+      //MvcView view = new ShowTextView("Show Text");
+      //MvcModel model = new ShowTextModel();
+      //Control control = newControl(view, model);
 
-      control.showView(true);
+      //control.showView(true);
    }
 
    public static void main(String[] args) {
