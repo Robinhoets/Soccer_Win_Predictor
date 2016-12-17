fbec54efc65310ca12a5334db78abfa23c32c5ee
Robinhoets <robertets88@gmail.com>
2016-12-16 23:44:19 -0500

    View is stacked vertically now

--- View.java
+++ View.java
@@ -67,7 +67,7 @@ public class View implements InterfaceView {
             pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         }
 
-		pane.setLayout(new GridLayout());
+		pane.setLayout(new GridBagLayout());
 
 		 JButton button;
 		 GridBagConstraints c = new GridBagConstraints();
@@ -77,47 +77,23 @@ public class View implements InterfaceView {
 	    }
 	 
 	    button = new JButton("Button 1");
-	    if (true) {
-	    c.weightx = 0.5;
-	    }
-	    c.fill = GridBagConstraints.NORTH;
+	    c.fill = GridBagConstraints.HORIZONTAL;
 	    c.gridx = 0;
 	    c.gridy = 0;
 	    pane.add(button, c);
 
 	    button = new JButton("Button 2");
-	    c.fill = GridBagConstraints.NORTH;
-	    c.weightx = 0.5;
-	    c.gridx = 1;
-	    c.gridy = 0;
-	    pane.add(button, c);
-	 
-	    button = new JButton("Button 3");
-	    c.fill = GridBagConstraints.NORTH;
-	    c.weightx = 0.5;
-	    c.gridx = 2;
-	    c.gridy = 0;
-	    pane.add(button, c);
-	 
-	    button = new JButton("Long-Named Button 4");
-	    c.fill = GridBagConstraints.NORTH;
-	    c.ipady = 40;      //make this component tall
-	    c.weightx = 0.0;
-	    c.gridwidth = 3;
+	    c.fill = GridBagConstraints.HORIZONTAL;
 	    c.gridx = 0;
 	    c.gridy = 1;
 	    pane.add(button, c);
 	 
-	    button = new JButton("5");
-	    c.fill = GridBagConstraints.NORTH;
-	    c.ipady = 0;       //reset to default
-	    c.weighty = 1.0;   //request any extra vertical space
-	    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-	    c.insets = new Insets(10,0,0,0);  //top padding
-	    c.gridx = 1;       //aligned with button 2
-	    c.gridwidth = 2;   //2 columns wide
-	    c.gridy = 2;       //third row
+	    button = new JButton("Button 3");
+	    c.fill = GridBagConstraints.HORIZONTAL;
+	    c.gridx = 0;
+	    c.gridy = 2;
 	    pane.add(button, c);
+	
 
 	}
 
