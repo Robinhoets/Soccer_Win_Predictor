1ef51dca71d82fb080221e2f4a06990fd2e69902
Robinhoets <robertets88@gmail.com>
2016-12-16 22:36:17 -0500

    View works with gridbag constraints now

--- View.java
+++ View.java
@@ -27,6 +27,11 @@ public class View implements InterfaceView {
 
 	public View(String title) {
 
+		//Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+		/*
+
 		jlbl1.setText("possession: ");
 		jlbl1.setVerticalTextPosition(JLabel.BOTTOM);
         jlbl1.setHorizontalTextPosition(JLabel.CENTER);
@@ -52,8 +57,70 @@ public class View implements InterfaceView {
       	//frame.getContentPane().add(statusBar.getComponent(), BorderLayout.PAGE_END);
      
      	frame.add(panel);
+		*/
+	}
+	final static boolean RIGHT_TO_LEFT = false;
+
+	public static void addComponentsToPane(Container pane) {
+
+		if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
+
+		pane.setLayout(new GridLayout());
+
+		 JButton button;
+		 GridBagConstraints c = new GridBagConstraints();
+	    if (false) {
+	    //natural height, maximum width
+	    c.fill = GridBagConstraints.NORTH;
+	    }
+	 
+	    button = new JButton("Button 1");
+	    if (true) {
+	    c.weightx = 0.5;
+	    }
+	    c.fill = GridBagConstraints.NORTH;
+	    c.gridx = 0;
+	    c.gridy = 0;
+	    pane.add(button, c);
+
+	    button = new JButton("Button 2");
+	    c.fill = GridBagConstraints.NORTH;
+	    c.weightx = 0.5;
+	    c.gridx = 1;
+	    c.gridy = 0;
+	    pane.add(button, c);
+	 
+	    button = new JButton("Button 3");
+	    c.fill = GridBagConstraints.NORTH;
+	    c.weightx = 0.5;
+	    c.gridx = 2;
+	    c.gridy = 0;
+	    pane.add(button, c);
+	 
+	    button = new JButton("Long-Named Button 4");
+	    c.fill = GridBagConstraints.NORTH;
+	    c.ipady = 40;      //make this component tall
+	    c.weightx = 0.0;
+	    c.gridwidth = 3;
+	    c.gridx = 0;
+	    c.gridy = 1;
+	    pane.add(button, c);
+	 
+	    button = new JButton("5");
+	    c.fill = GridBagConstraints.NORTH;
+	    c.ipady = 0;       //reset to default
+	    c.weighty = 1.0;   //request any extra vertical space
+	    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+	    c.insets = new Insets(10,0,0,0);  //top padding
+	    c.gridx = 1;       //aligned with button 2
+	    c.gridwidth = 2;   //2 columns wide
+	    c.gridy = 2;       //third row
+	    pane.add(button, c);
 
 	}
+
 
 	@Override
    	public void setVisible(boolean visible) {
