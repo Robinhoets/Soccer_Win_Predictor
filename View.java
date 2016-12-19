975f4c8847cde16d921df39299390b7ed21abbb4
Robinhoets <robertets88@gmail.com>
2016-12-19 14:25:03 -0500

    The initial GUI for View now set up by seperate class ViewDisplayText.java

--- View.java
+++ View.java
@@ -15,13 +15,16 @@ public class View implements InterfaceView {
 
     private Calculated calcSuccess = new Calculated();
 
+    ViewDisplayText dislpayText = new ViewDisplayText();
+
 	public View() {
 		//Set up the content pane.
-        addComponentsToPane(frame.getContentPane());
+        //addComponentsToPane(frame.getContentPane());
         // close when exited
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.getContentPane().add(calcSuccess.getComponent(), GridBagConstraints.HORIZONTAL);
+		//frame.getContentPane().add(calcSuccess.getComponent(), GridBagConstraints.HORIZONTAL);
+		frame.getContentPane().add(dislpayText.getMainComponent(), BorderLayout.CENTER);
 	}
 
 	// set up our initial view
@@ -96,12 +99,54 @@ public class View implements InterfaceView {
 class ViewDisplayText {
 
 	private JPanel mainPanel = new JPanel();
-	private JButton addRecordButton = new JButton();
-	private JButton calculateButton = new JButton();
+	private JButton addRecordButton = new JButton("Add Record");
+	private JButton calculateButton = new JButton("Calculte");
+	private JTextArea textArea = new JTextArea(20,20);
+
+	private JLabel possessionLabel = new JLabel("POSSESSION");
+	private JLabel shotsLabel = new JLabel("SHOTS");
+	private JLabel passAccLabel = new JLabel("PASS ACCURACY");
+
+	private JTextField possessionText = new JTextField(10);
+	private JTextField shotsText = new JTextField(10);
+	private JTextField passAccuracyText = new JTextField(10);
 
 	public ViewDisplayText() {
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 0, 2, 0));
+		buttonPanel.add(addRecordButton);
+		buttonPanel.add(calculateButton);
 
+		JPanel textAndLabelPanel = new JPanel(new GridLayout(2, 0, 2, 0));
+		textAndLabelPanel.add(possessionLabel);
+		textAndLabelPanel.add(shotsLabel);
+		textAndLabelPanel.add(passAccLabel);
+
+		textAndLabelPanel.add(possessionText);
+		textAndLabelPanel.add(shotsText);
+		textAndLabelPanel.add(passAccuracyText);
+
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(textAndLabelPanel, BorderLayout.PAGE_START);
+      	mainPanel.add(new JScrollPane(textArea), BorderLayout.CENTER);
+      	mainPanel.add(buttonPanel, BorderLayout.PAGE_END);
 	}
+
+	public JComponent getMainComponent() {
+		return mainPanel;
+	}
+
+	public void setAddRecordAction(Action action) {
+		addRecordButton.setAction(action);
+	}
+
+	public String getTextAreaText() {
+      return textArea.getText();
+    }
+
+   	public void setTextAreaText(String text) {
+   	   textArea.setText(text);
+   	}
 
 
 }
