c9e99ae1179eb2cd3b4fe987ac7418acde7b1c63
Robinhoets <robertets88@gmail.com>
2016-12-22 14:37:22 -0500

    Upadted Game for more comprehensive list of statistics.

--- Game.java
+++ Game.java
@@ -1,32 +1,171 @@
 
 
 public class Game{
+
+	private int yellowCards, redCards, fouls, 
+				corners, offsides, possessionPercent, passAccuracyPercent, 
+				formation, opponentsFormation,
+				goals, goalsScoredCounterAttack, goalsScoredOpenPlay, goalsScoredSetPiece, goalsScoredPenalty, ownGoals, 
+				shots, shotsOnTarget, dribblesWon, foulsWon,
+				tackles, interceptions, clearance, shotsBlocked, shotsConceded,
+				totalPasses, passSuccessPercent, crossPass, throughBallPass, longBallPass, shortPass;
 	
-	private int possession;
-	private int shots;
-	private int passAccuracy;
 
-	public Game(int possession, int shots, int passAccuracy) {
-		this.possession = possession;
+	public Game(int yellowCards, int redCards, int fouls,
+				int corners, int offsides, int possessionPercent, int passAccuracyPercent,
+				int formation, int opponentsFormation,
+				int goals, int goalsScoredCounterAttack, int goalsScoredOpenPlay, int goalsScoredSetPiece, int goalsScoredPenalty, int ownGoals,
+				int shots, int shotsOnTarget, int dribblesWon, int foulsWon,
+				int tackles, int interceptions, int clearance, int shotsBlocked, int shotsConceded,
+				int totalPasses, int passSuccessPercent, int crossPass, int throughBallPass, int longBallPass, int shortPass) 
+	{
+		this.yellowCards = yellowCards;
+		this.redCards = redCards;
+		this.fouls = fouls;
+
+		this.corners = corners;
+		this.offsides = offsides;
+		this.possessionPercent = possessionPercent;
+		this.passAccuracyPercent = passAccuracyPercent;
+
+		this.formation = formation;
+		this.opponentsFormation = opponentsFormation;
+
+		this.goals = goals;
+		this.goalsScoredCounterAttack = goalsScoredCounterAttack;
+		this.goalsScoredOpenPlay = goalsScoredOpenPlay;
+		this.goalsScoredSetPiece = goalsScoredSetPiece;
+		this.goalsScoredPenalty = goalsScoredPenalty;
+		this.ownGoals = ownGoals;
+
 		this.shots = shots;
-		this.passAccuracy = passAccuracy;
+		this.shotsOnTarget = shotsOnTarget;
+		this.dribblesWon = dribblesWon;
+		this.foulsWon = foulsWon;
+
+		this.tackles = tackles;
+		this.interceptions = interceptions;
+		this.clearance = clearance;
+		this.shotsBlocked = shotsBlocked;
+		this.shotsConceded = shotsConceded;
+
+		this.totalPasses = totalPasses;
+		this.passSuccessPercent = passSuccessPercent;
+		this.crossPass = crossPass;
+		this.throughBallPass = throughBallPass;
+		this.longBallPass = longBallPass;
+		this.shortPass = shortPass;
 	}
 
-
-	public int getPossession(){
-		return possession;
+	// Cards and fouls section
+	public int getYellowCards() {
+		return yellowCards;
+	}
+	public int getRedCards() {
+		return redCards;
+	}
+	public int getFouls() {
+		return fouls;
 	}
 
+	// corners, offsides, possession, passing
+	public int getCorners() {
+		return corners;
+	}
+	public int getOffsides() {
+		return offsides;
+	}
+	public int getPossessionPercent() {
+		return possessionPercent;
+	}
+	public int getPassAccuracyPercent() {
+		return passAccuracyPercent;
+	}
+
+	// tactical formation
+	public int getFormation() {
+		return formation;
+	}
+	public int getOpponentsFormation() {
+		return opponentsFormation;
+	}
+
+	// goals formation
+	public int getGoals() {
+		return goals;
+	}
+	public int getGoalsScoredCounterAttack() {
+		return goalsScoredCounterAttack;
+	}
+	public int getGoalsScoredOpenPlay() {
+		return goalsScoredOpenPlay;
+	}
+	public int getGoalsScoredSetPiece() {
+		return goalsScoredSetPiece;
+	}
+	public int getGoalsScoredPenalty() {
+		return getGoalsScoredPenalty;
+	}
+
+	// offensive moves
 	public int getShots(){
 		return shots;
 	}
+	public int getShotsOnTarget() {
+		return shotsOnTarget;
+	}
+	public int getDribblesWon() {
+		return dribblesWon;
+	}
+	public int getFoulsWon() {
+		return foulsWon;
+	}
 
-	public int getPassAccuracy() {
-		return passAccuracy;
+	// defensive moves
+	public int getTackles() {
+		return tackles;
+	}
+	public int getInterceptions() {
+		return interceptions;
+	}
+	public int getClearance() {
+		return clearance;
+	}
+	public int getShotsBlocked() {
+		return shotsBlocked;
+	}
+	public int getShotsConceded() {
+		return shotsConceded;
+	}
+
+	// passing
+	public int getTotalPasses() {
+		return totalPasses;
+	}
+	public int getPassSuccessPercent() {
+		return passSuccessPercent;
+	}
+	public int getCrossPass() {
+		return crossPass;
+	}
+	public int getThroughBallPass() {
+		return throughBallPass;
+	}
+	public int getLongBallPass() {
+		return longBallPass;
+	}
+	public int getShortPass() {
+		return shortPass;
 	}
 
 	public String toString() {
-		return possession + " " + shots + " " + passAccuracy;
+		return yellowCards + " " + redCards + " " + fouls + "\n" + 
+				corners + " " + offsides + " " + possessionPercent + " " + passAccuracyPercent + "\n" + 
+				formation + " " + opponentsFormation + "\n" + 
+				goals + " " + goalsScoredCounterAttack + " " + goalsScoredOpenPlay + " " + goalsScoredSetPiece + " " + goalsScoredPenalty + " " + ownGoals + "\n" + 
+				shots + " " + shotsOnTarget + " " + dribblesWon + " " + foulsWon + "\n" + 
+				tackles + " " + interceptions + " " + clearance + " " + shotsBlocked + " " + shotsConceded + "\n" + 
+				totalPasses + " " + passSuccessPercent + " " + crossPass + " " + throughBallPass + " " + longBallPass + " " + shortPass;
 	}
 	
 }
