5d32582916e4e17964e847b8e24bad5cc2d28788
Robinhoets <robertets88@gmail.com>
2017-01-04 19:27:20 -0500

    Created RecordText.java to add Scrollpane of records to main view - seperation of classes.:

---
+++ RecordText.java
@@ -0,0 +1,23 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.Window;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
+
+
+
+
+class RecordText{
+
+
+	public RecordText() {
+
+
+		
+	}
+	
+
+
+}
