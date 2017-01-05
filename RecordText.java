b2dec6b36a21ac1ba50f93a2614fbd5ade850829
Robinhoets <robertets88@gmail.com>
2017-01-04 19:59:01 -0500

    Added methods to Records text to create and get components.

--- RecordText.java
+++ RecordText.java
@@ -11,13 +11,39 @@ import java.awt.*;
 
 class RecordText{
 
+	//create scrollpane that can be added
+	JTextArea textArea = new JTextArea(5, 70);
+	JScrollPane scrollPane = new JScrollPane(textArea);
+
 
 	public RecordText() {
 
 
-		
+
 	}
 	
+	public void  addGameListToScrollPane(ArrayList<String> gameList) {
 
+    	textArea.setText("");
+    	for(int i=0; i<gameList.size(); i++) {  		
+    		textArea.append(gameList.get(i));	
+    	}
+
+    }
+
+    public void addScrollPane(){
+
+    	scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+   	 	scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+    }
+
+    public JScrollPane getScrollPane() {
+    	return scrollPane;
+    }
+
+    public JTextArea getTextArea() {
+    	return textArea;
+    }
 
 }
