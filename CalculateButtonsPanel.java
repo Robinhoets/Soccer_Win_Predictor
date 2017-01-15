aa1755c97e204eb1fb553a2601957035679ea56f
Robinhoets <robertets88@gmail.com>
2017-01-15 12:25:34 -0500

    Calculate button now displays check boxes to pick dependent variables.

--- CalculateButtonsPanel.java
+++ CalculateButtonsPanel.java
@@ -2,25 +2,44 @@ import java.awt.Window;
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
 import java.awt.*;
+import java.awt.event.*;
 
 public class CalculateButtonsPanel {
 
 	private static JPanel mainPanel = new JPanel(new BorderLayout(3,3));
 	private JPanel buttonPanel = new JPanel(new GridLayout(3, 0, 2, 0));
-	//private JTextArea textArea;
-	//private JScrollPane scrollPane;
+	private JPanel checkBoxesPanel = new JPanel(new GridLayout(3, 0, 2, 0));
+
 	private JButton goBackToMainPageButton = new JButton("Main Page");
 	private JButton linearRegressionButton = new JButton("Linear Regression");
+
+	private JCheckBox yellowCardsBox;
+	private JCheckBox redCardsBox;
 
 	public CalculateButtonsPanel() {
 		buttonPanel.add(goBackToMainPageButton);
 		buttonPanel.add(linearRegressionButton);
+
+		yellowCardsBox = new JCheckBox("Yellow Cards");
+		yellowCardsBox.setMnemonic(KeyEvent.VK_Y);
+        yellowCardsBox.setSelected(true);
+
+		redCardsBox = new JCheckBox("Red Cards");
+		redCardsBox.setMnemonic(KeyEvent.VK_R);
+        redCardsBox.setSelected(true);
+
+        //yellowCardsBox.addItemListener(this);
+        //redCardsBox.addItemListener(this);
+
+        checkBoxesPanel.add(yellowCardsBox);
+        checkBoxesPanel.add(redCardsBox);
 
 	}
 
 
 	public JPanel addPanel() {
 		mainPanel.add(buttonPanel,BorderLayout.CENTER);
+		mainPanel.add(checkBoxesPanel, BorderLayout.SOUTH);
 		return mainPanel;
 	}
 
